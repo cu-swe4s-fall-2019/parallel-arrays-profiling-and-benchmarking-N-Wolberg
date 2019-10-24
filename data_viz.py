0e880bf854afd0919f4bb48f76cab69adb766dd0
Nathan Wolberg <nawo3532@colorado.edu>
2019-10-24 23:44:13 +0000

    Add boxplot tests

--- data_viz.py
+++ data_viz.py
@@ -4,14 +4,26 @@ import math_lib
 matplotlib.use('Agg')
 #With helpful permission from Chi-Ju Wu
 
-def boxplot(par, meta, x_label, y_label, title, out_file):
-    """plot boxplot for an input array and save the result as a png file
+
+
+def boxplot(data, meta, x_label, y_label, title, out_file):
     """
-    plt.boxplot(par)
-    plt.ylabel('Distribution')
-    plt.xlabel('Box')
-    plt.savefig(out_file)
-    pass
+    plot boxplot for input parallel array and save the result as a png file
+    """
+    fig, ax1 = plt.subplots(figsize=(12, 8))
+
+    ax1.boxplot(data)
+
+    # Hide these grid behind plot objects
+    ax1.set_axisbelow(True)
+    ax1.set_title(title)
+    ax1.set_xlabel(x_label)
+    ax1.set_ylabel(y_label)
+
+    # set tick labels and export the result
+    ax1.set_xticklabels(meta, rotation=90, fontsize=8)
+    fig.savefig(out_file, bbox_inches='tight')
+
 
 def histogram(L, out_file_name):
     """plot boxplot for an input array and save the result as a png file
