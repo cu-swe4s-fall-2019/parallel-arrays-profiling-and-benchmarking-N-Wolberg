1b1202ec17da9cdd9b45ea27142e760a77e28285
Nathan Wolberg <nawo3532@colorado.edu>
2019-10-24 23:49:30 +0000

    Parse args in plot_gtex.py

--- plot_gtex.py
+++ plot_gtex.py
@@ -1,4 +1,48 @@
 import data_viz
+import gzip
+import sys
+import os
+import argparse
+
+
+# parse arguments
+def parse_args():
+    parser = argparse.ArgumentParser(
+        description='Takes .',
+        prog='plot_gtex.py')
+
+    # require file name as one of the inputs
+    parser.add_argument('--output_file',
+                        type=str,
+                        help='Name of the output plot',
+                        required=True)
+
+    # require group type as one of the inputs
+    parser.add_argument('--group_type',
+                        type=str,
+                        help='The group type. e.g. SMTS',
+                        required=True)
+
+    # require gene as one of the inputs
+    parser.add_argument('--gene',
+                        type=str,
+                        help='Target gene. e.g. ACTA2',
+                        required=True)
+
+    # require meta file as one of the inputs
+    parser.add_argument('--sample_attributes',
+                        type=str,
+                        help='A txt file containing meta data',
+                        required=True)
+
+    # require gene data as one of the inputs
+    parser.add_argument('--gene_reads',
+                        type=str,
+                        help='Database of genes',
+                        required=True)
+
+    return parser.parse_args()
+
 
 def linear_search(key, L):
     pass
